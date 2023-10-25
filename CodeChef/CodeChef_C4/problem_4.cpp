ea7afc61ee642595b5d4572713ef85a0cbb3b7f4
srayo16 <srayosikder@gmail.com>
2023-10-25 22:58:48 +0600

    D

---
+++ CodeChef/CodeChef_C4/problem_4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n - 1 << " " << n << endl;
+    }
+    return 0;
+}
